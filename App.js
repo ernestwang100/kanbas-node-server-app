831c2c0a4e1aae2e43b07d95701e093b35a3cce3
Yu-En Wang <ernestwang100@gmail.com>
2024-04-04 18:43:46 -0700

    a6 Mongo su23 3.5.3

--- App.js
+++ App.js
@@ -5,13 +5,17 @@ import cors from 'cors';
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(cors());
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 
 Lab5(app);
 Hello(app);
